56202b9f849f1b4a82fc33d3c19ba02c45c4c30b
Jürgen De Lira <106788934+JurgenDeLira@users.noreply.github.com>
2024-07-03 11:32:18 -0700

    Add files via upload

---
+++ Videojuego2.py
@@ -0,0 +1,14 @@
+class Videojuego(self, character, points, lifepoints):
+    def __init__():
+        self.character = character
+        self.points = int(points)
+        self.lifepoints = lifepoints
+
+def gainLifepoints(self, lifepointsUp, lifepoints):
+    if lifepoints > 6:
+        return
+    lifepoints = lifepoints + 1
+    return lifepoints
+
+def lowLifepoints(self, lifepointsDown, lifepoints):
+    if life
